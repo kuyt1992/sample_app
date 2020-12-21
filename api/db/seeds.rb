5cb5a6f6fd7dd660ee8ce8504b2d3763112e3365
kuyt1992 <kuyt1992@gmail.com>
2020-12-22 00:08:16 +0900

    add: Postモデルのseedファイル作成

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.create([
+	{content: "Hello World!"},
+	{content: "test post"},
+	{content: "Good Night"}
+])
