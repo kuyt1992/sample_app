915383256c3feb92c5f15d4d0d31fd4a1bba000c
kuyt1992 <kuyt1992@gmail.com>
2020-12-29 14:46:48 +0900

    fix: PostControllerのインデントを修正

--- api/app/controllers/posts_controller.rb
+++ api/app/controllers/posts_controller.rb
@@ -1,28 +1,27 @@
 class PostsController < ApplicationController
 
-	def index
-		@post = Post.all
-		render json: @post
-	end
-
-	def create
-		@post = Post.create(content: params[:content])
+  def index
+    @post = Post.all
     render json: @post
-	end
+  end
 
-	def update
-		@post = Post.find(params[:id])
-		@post.update_attributes(content: params[:content])
-		render json: @post
-	end
+  def create
+    @post = Post.create(content: params[:content])
+    render json: @post
+  end
 
-	def destroy
-		@post = Post.find(params[:id])
-		if @post.destroy
-			head :no_content, status: :ok
-		else
-			render json: @issue.errors, status: :unprocessable_entity
-		end
-	end
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(content: params[:content])
+    render json: @post
+  end
 
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      head :no_content, status: :ok
+    else
+      render json: @issue.errors, status: :unprocessable_entity
+    end
+  end
 end
