9d5a44d5621085c68f8e07593508fda812fc4969
kuyt1992 <kuyt1992@gmail.com>
2021-01-03 23:27:22 +0900

    feat: Userコントローラー雛形作成

---
+++ api/app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+  def index
+    @user = User.all
+    render json: @user
+  end
+
+  def create
+    @user = User.create(user_params)
+    render json: @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    render json: @user
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      head :no_content, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :name, :email)
+  end
+end
