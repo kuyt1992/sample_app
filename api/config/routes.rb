73a28621c6cbf05b5b608fdd000e5309077a12ee
kuyt1992 <kuyt1992@gmail.com>
2021-01-05 23:55:37 +0900

    feat: devise導入に伴い、routing設定

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      registrations: 'api/auth/registrations'
+    }
+  end
   resources :users
   resources :posts
 end
