e12c38dbf48fb9c78d627c93d12aa75906d7927f
kuyt1992 <kuyt1992@gmail.com>
2021-01-05 23:56:28 +0900

    update: user controllerをコメアウト

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,33 +1,32 @@
 class UsersController < ApplicationController
+  # def index
+  #   @user = User.all
+  #   render json: @user
+  # end
 
-  def index
-    @user = User.all
-    render json: @user
-  end
+  # def create
+  #   @user = User.create(user_params)
+  #   render json: @user
+  # end
 
-  def create
-    @user = User.create(user_params)
-    render json: @user
-  end
+  # def update
+  #   @user = User.find(params[:id])
+  #   @user.update(user_params)
+  #   render json: @user
+  # end
 
-  def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    render json: @user
-  end
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   if @user.destroy
+  #     head :no_content, status: :ok
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  def destroy
-    @user = User.find(params[:id])
-    if @user.destroy
-      head :no_content, status: :ok
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  # private
 
-  private
-
-  def user_params
-    params.require(:user).permit(:id, :name, :email)
-  end
+  # def user_params
+  #   params.require(:user).permit(:id, :name, :email)
+  # end
 end
