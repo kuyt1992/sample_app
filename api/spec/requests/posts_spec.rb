05259c5313d4ff1ac4870943e2e36791977000fe
kuyt1992 <kuyt1992@gmail.com>
2021-01-14 18:00:24 +0900

    fix: rubocop修正

--- api/spec/requests/posts_spec.rb
+++ api/spec/requests/posts_spec.rb
@@ -10,14 +10,14 @@ RSpec.describe "Posts", type: :request do
 
     it "if getting posts list" do
       get posts_path
-      expect(response).to be_success
+      expect(response).to be_successful
       expect(response).to have_http_status(200)
     end
 
-    it "投稿内容が表示されていること" do
+    it "投稿一覧を取得できる" do
       json = JSON.parse(response.body)
-      expect(json).to match(/test1/)
-      expect(json).to match(/テスト投稿2/)
+      expect(json[0]["content"]).to eq @post1.content
+      expect(json[1]["content"]).to eq @post2.content
     end
   end
 end
